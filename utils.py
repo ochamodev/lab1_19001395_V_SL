ee2fd9aba2253bfcdd9b12e9516383c1030a4934
ochamo <cheleyotto98@gmail.com>
2024-05-18 22:07:29 -0600

    feat: convert day columns to a single one

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 
 import calendar
-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 def findDay(year, month, day):
     
     dayNumber = calendar.weekday(year, month, day)
@@ -8,3 +11,15 @@ def findDay(year, month, day):
     days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
      
     return days[dayNumber]
+
+
+def cyclical_encoding(data, col: str, max_val: float):
+    data[col + '_sin'] = np.sin(2 * np.pi * data[col] / max_val)
+    data[col + '_cos'] = np.sin(2 * np.pi * data[col] /  max_val)
+    return data
+
+def plot_columns(ds: pd.DataFrame):
+    columns = ds.columns
+    for i, col in enumerate(columns):
+        plt.figure(i)
+        sns.histplot(ds[col], kde=True)
